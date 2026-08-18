a1c6fb14e672d1f31de61f529974c770af5427be
senadalmeidapro <senadalmeidapro@gmail.com>
2026-08-18 20:14:54 +0200

    commit

---
+++ src/components/UserCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isDisabled = ref(true)
+
+function toggleDisabled() {
+  isDisabled.value = !isDisabled.value
+}
+</script>
+
+<template>
+  <section>
+    <button :disabled="isDisabled">
+      Submit
+    </button>
+
+    <button @click="toggleDisabled">
+      Toggle disabled
+    </button>
+  </section>
+</template>
